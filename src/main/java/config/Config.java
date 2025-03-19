db237635ec3adff7ce7b767c28736690ec16990e
shailesh <shaileshk@sigmoidanalytics.com>
2025-03-19 20:59:48 +0530

    code jdbc

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -3,8 +3,6 @@ package config;
 public class Config {
     public String csvFilePath;
     public String dbUrl;
-    public String dbUser;
-    public String dbPassword;
     public String outputPath;
 }
 
--- src/main/java/config/ConfigLoader.java
+++ src/main/java/config/ConfigLoader.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConfigLoader {
     private static Config config;
@@ -15,5 +19,28 @@ public class ConfigLoader {
             config = mapper.readValue(new File(path), Config.class);
         }
         return config;
+    }
+
+    public static void initializeDatabase() {
+        try (Connection conn = DriverManager.getConnection(config.dbUrl);
+             Statement stmt = conn.createStatement()) {
+            if (conn != null) {
+                String sql = "CREATE TABLE IF NOT EXISTS tradingData (" +
+                        "TransactionID INTEGER PRIMARY KEY," +
+                        "Date TEXT," +
+                        "Time TEXT," +
+                        "StockSymbol TEXT," +
+                        "Action TEXT," +
+                        "Quantity INTEGER," +
+                        "Price REAL," +
+                        "Name TEXT," +
+                        "Kerboros TEXT NOT NULL," +
+                        "Comments TEXT" +
+                        ");";
+                stmt.execute(sql);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
